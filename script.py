2ae7a7a902329d8ae73bdf827c1a9c0d99fcec85
Chaitanya  Jitendrasing  Girase <45315166+chaitanyasisodiya@users.noreply.github.com>
2023-08-06 13:34:44 +0530

    removed script.py

--- script.py
+++
@@ -1,4 +0,0 @@
-
-MLFLOW_TRACKING_URI=https://dagshub.com/chaitanyasisodiya/End-to-End-MLops.mlflow \
-MLFLOW_TRACKING_USERNAME=chaitanyasisodiya \
-MLFLOW_TRACKING_PASSWORD=8af8794ef1feeba128c8ee6534fe384d81f4888a \
