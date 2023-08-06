44fa6c8539e5edc0f308077f08fee58cf5eb425e
Chaitanya  Jitendrasing  Girase <45315166+chaitanyasisodiya@users.noreply.github.com>
2023-08-06 12:16:21 +0530

    files updated

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -100,7 +100,7 @@ class ConfigurationManager:
             root_dir=config.root_dir,
             test_data_path=config.test_data_path,
             model_path = config.model_path,
-            all_params=params,
+            all_params=params, 
             metric_file_name = config.metric_file_name,
             target_column = schema.name,
             mlflow_uri="https://dagshub.com/entbappy/End-to-end-Machine-Learning-Project-with-MLflow.mlflow",
